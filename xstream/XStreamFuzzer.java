39dcb564de959424bb6a2f5787f530772fc5fb36
flruee <florian.rueegsegger@gmail.com>
2021-12-16 16:15:58 +0000

    added project folder, deleted old shit

--- xstream/XStreamFuzzer.java
+++
@@ -1,9 +0,0 @@
-import com.thoughtworks.xstream.XStream;
-
-public class XStreamFuzzer {
-    public static void fuzzerTestOneInput(byte[] input){
-        XStream xstream = new XStream();
-      	String s = xstream.toXML(input);
-	
-	}
-}
